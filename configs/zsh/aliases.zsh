21d5ea19655d369347ea1ff96d1532a73c4a667d
Chris Bloom <chrisbloom7@github.com>
2025-03-18 16:53:39 -0400

    lazy eval dotfiles

--- configs/zsh/aliases.zsh
+++ configs/zsh/aliases.zsh
@@ -17,7 +17,7 @@ if [[ "$(uname -a)" =~ "Darwin" ]]; then
 fi
 
 # Directories
-alias dotfiles="cd \"${DOTFILES:?DOTFILES not set}\""
+alias dotfiles="cd \"\${DOTFILES:?DOTFILES not set}\""
 src () { cd "$HOME/src/$@" }
 
 # Python
@@ -26,9 +26,9 @@ alias py="python3"
 alias pip="pip3"
 
 # Projects and files
-alias edots="${EDITOR:-code} \"${DOTFILES}\""
-alias ehosts="${EDITOR:-code} /etc/hosts"
-alias edotaliases="${EDITOR:-code} ${HOME}/.aliases"
+alias edots="${EDITOR} \"${DOTFILES}\""
+alias ehosts="${EDITOR} /etc/hosts"
+alias edotaliases="${EDITOR} ${HOME}/.aliases"
 readme () {
   if [[ -z "$(command -v pandoc 2>/dev/null)" ]] || [[ -z "$(command -v lynx 2>/dev/null)" ]]; then
     return 1;
